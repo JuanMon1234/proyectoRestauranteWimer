3a8c71c63c879f9ab835dbcf4ff4008e38085549
juanitodeveloper <fernandomontilla8@gmail.com>
2025-11-06 11:34:22 -0500

    a

--- include/conex.php
+++ include/conex.php
@@ -3,7 +3,7 @@ namespace Include;
 
 class Conexion {
     public static function crear() {
-        $conexion = mysqli_connect("localhost", "usuario", "contraseña", "basedatos");
+        $conexion = mysqli_connect("localhost", "usuario", "clave", "basedatos");
         if (!$conexion) {
             die("Error de conexión: " . mysqli_connect_error());
         }
