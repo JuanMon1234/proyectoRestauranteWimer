db071fa0dc271e2ac34c4d39e65c8afa781c98a1
juanitodeveloper <fernandomontilla8@gmail.com>
2025-11-17 19:23:50 -0500

    1

--- include/conex.php
+++ include/conex.php
@@ -9,12 +9,7 @@ class Conexion
 
         $conexion = mysqli_connect(
             $config['DB_HOST'],
-            $config['DB_USER'], 
-            $config['DB_PASS'],
-            $config['DB_NAME'],
-            $config['DB_PORT'],
-        );
-
+            $config['DB_USER'], $config['DB_PASS'],$config['DB_NAME'],$config['DB_PORT'],);
         if (!$conexion) {
             die("❌ Error de conexión: " . mysqli_connect_error());
         }
