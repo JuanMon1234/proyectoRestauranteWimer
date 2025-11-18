2fae23dd936654aff31abee4a36b319b0ed38e84
juanitodeveloper <fernandomontilla8@gmail.com>
2025-11-17 22:44:16 -0500

    12

--- archivos/controlador/recuperarclave.php
+++ archivos/controlador/recuperarclave.php
@@ -1,6 +1,6 @@
 <?php
 include_once "../../include/conex.php";
-include_once "../../include/autoload.php";
+include_once ("../../include/autoload.php");
 require_once '../../herramientas/PHPMailer/src/PHPMailer.php';
 require_once '../../herramientas/PHPMailer/src/SMTP.php';
 require_once '../../herramientas/PHPMailer/src/Exception.php';
@@ -9,7 +9,7 @@ use IncludeDB\Conexion;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-$env = include_once "../../include/env.php";
+$env = include_once ("../../include/env.php");
 header('Content-Type: application/json');
 
 $link = Conexion::conexion();
